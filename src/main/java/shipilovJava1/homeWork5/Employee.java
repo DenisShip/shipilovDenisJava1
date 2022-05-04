ce115c988233c4610acb5e3a203baae594b54983
DenisShip <90905485+DenisShip@users.noreply.github.com>
2022-05-04 23:18:28 +0300

    I did my homework5 with you comments / не могу понять как распечатать информацию о обьекте public void print ()                   System.out.println("name, position, email, phone,salary,age");

--- src/main/java/shipilovJava1/homeWork5/Employee.java
+++ src/main/java/shipilovJava1/homeWork5/Employee.java
@@ -2,12 +2,13 @@ package shipilovJava1.homeWork5;
 
 public class Employee {
 
-    private String name;
-    private String position;
-    private String email;
-    private String phone;
-    private int salary;
-    private int age;
+
+    private final String name;
+    private final String position;
+    private final String email;
+    private final String phone;
+    private final int salary;
+    private final int age;
 
 
     public Employee(String name, String position, String email, String phone, int salary, int age) {
@@ -18,40 +19,39 @@ public class Employee {
         this.salary = salary;
         this.age = age;
 
+    }
+    public void print (){
+        System.out.println("name, position, email, phone,salary,age");
 
     }
 
-    public static void main(String[] args) {
-        printInfoEmployee();
-
-
-
+    public Employee(String name, String position, String phone, int salary, int age) {
+        this(name, position, "", phone, salary, age);
     }
 
+    public String getName() {
+        return name;
+    }
 
-    public static void printInfoEmployee() {
+    public String getPosition() {
+        return position;
+    }
 
-        Employee[] employeeArray = new Employee[5];
-        employeeArray[0] = new Employee("Иван Иванов ", "менеджер", "i@com.ru", "+7928555667", 500, 43);
-        employeeArray[1] = new Employee("Петр Петров", "водитель", "p@com.ru", "+79285556668", 300, 23);
-        employeeArray[2] = new Employee("Семен Семенов", "сторож", "s@com.ru", "+7928555669", 250, 21);
-        employeeArray[3] = new Employee("Максим Максимов", "диреткор", "m@com.ru", "+7928555661", 1000, 49);
-        employeeArray[4] = new Employee("Андрей Андреев", "юрист", "a@com.ru", "+7928555662", 400, 28);
+    public String getEmail() {
+        return email;
+    }
 
+    public String getPhone() {
+        return phone;
+    }
 
+    public int getSalary() {
+        return salary;
+    }
 
-        System.out.println("Сотрудник 1 = " + employeeArray[0].name + "; должность: " + employeeArray[0].position + "; почта: " + employeeArray[0].email + "; номер телефона: " + employeeArray[0].phone + "; зарплата: " + employeeArray[0].salary + "; возраст: " + employeeArray[0].age );
-        System.out.println("Сотрудник 2 = " + employeeArray[1].name + "; должность: " + employeeArray[1].position + "; почта: " + employeeArray[1].email + "; номер телефона: " + employeeArray[1].phone + "; зарплата: " + employeeArray[1].salary + "; возраст: " + employeeArray[1].age );
-        System.out.println("Сотрудник 3 = " + employeeArray[2].name + "; должность: " + employeeArray[2].position + "; почта: " + employeeArray[2].email + "; номер телефона: " + employeeArray[2].phone + "; зарплата: " + employeeArray[2].salary + "; возраст: " + employeeArray[2].age );
-        System.out.println("Сотрудник 4 = " + employeeArray[3].name + "; должность: " + employeeArray[3].position + "; почта: " + employeeArray[3].email + "; номер телефона: " + employeeArray[3].phone + "; зарплата: " + employeeArray[3].salary + "; возраст: " + employeeArray[3].age );
-        System.out.println("Сотрудник 5 = " + employeeArray[4].name + "; должность: " + employeeArray[4].position + "; почта: " + employeeArray[4].email + "; номер телефона: " + employeeArray[4].phone + "; зарплата: " + employeeArray[4].salary + "; возраст: " + employeeArray[4].age );
-
-        for (int i = 0; i < 4; i++) {
-            if (employeeArray[i].age > 43 ){
-                System.out.println(employeeArray[i].name);
-            }
-
-        }
-
+    public int getAge() {
+        return age;
     }
 }
+
+
--- src/main/java/shipilovJava1/homeWork5/HomeWork5.java
+++ src/main/java/shipilovJava1/homeWork5/HomeWork5.java
@@ -3,8 +3,28 @@ package shipilovJava1.homeWork5;
 public class HomeWork5 {
 
 
+    public static void main(String[] args) {
 
 
+        Employee[] employeeArray = new Employee[5];
+        employeeArray[0] = new Employee("Иван Иванов ", "менеджер", "i@com.ru", "+7928555667", 500, 43);
+        employeeArray[1] = new Employee("Петр Петров", "водитель", "p@com.ru", "+79285556668", 300, 23);
+        employeeArray[2] = new Employee("Семен Семенов", "сторож", "", "+7928555669", 250, 21);
+        employeeArray[3] = new Employee("Максим Максимов", "диреткор", "m@com.ru", "+7928555661", 1000, 49);
+        employeeArray[4] = new Employee("Андрей Андреев", "юрист", "a@com.ru", "+7928555662", 400, 28);
 
 
+        System.out.println("Сотрудник 1 = " + employeeArray[0].getName() + "; должность: " + employeeArray[0].getPosition() + "; почта: " + employeeArray[0].getEmail() + "; номер телефона: " + employeeArray[0].getPhone() + "; зарплата: " + employeeArray[0].getSalary() + "; возраст: " + employeeArray[0].getAge());
+        System.out.println("Сотрудник 2 = " + employeeArray[1].getName() + "; должность: " + employeeArray[1].getPosition() + "; почта: " + employeeArray[1].getEmail() + "; номер телефона: " + employeeArray[1].getPhone() + "; зарплата: " + employeeArray[1].getSalary() + "; возраст: " + employeeArray[1].getAge());
+        System.out.println("Сотрудник 3 = " + employeeArray[2].getName() + "; должность: " + employeeArray[2].getPosition() + "; почта: " + employeeArray[2].getEmail() + "; номер телефона: " + employeeArray[2].getPhone() + "; зарплата: " + employeeArray[2].getSalary() + "; возраст: " + employeeArray[2].getAge());
+        System.out.println("Сотрудник 4 = " + employeeArray[3].getName() + "; должность: " + employeeArray[3].getPosition() + "; почта: " + employeeArray[3].getEmail() + "; номер телефона: " + employeeArray[3].getPhone() + "; зарплата: " + employeeArray[3].getSalary() + "; возраст: " + employeeArray[3].getAge());
+        System.out.println("Сотрудник 5 = " + employeeArray[4].getName() + "; должность: " + employeeArray[4].getPosition() + "; почта: " + employeeArray[4].getEmail() + "; номер телефона: " + employeeArray[4].getPhone() + "; зарплата: " + employeeArray[4].getSalary() + "; возраст: " + employeeArray[4].getAge());
+
+        for (int i = 0; i < 4; i++) {
+            if (employeeArray[i].getAge() > 43) {
+                System.out.println(employeeArray[i].getName());
+            }
+
+        }
+    }
 }
