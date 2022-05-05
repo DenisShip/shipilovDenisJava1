97f3829f15f8a34d159996f695b7896c831f57e1
DenisShip <90905485+DenisShip@users.noreply.github.com>
2022-05-05 21:45:11 +0300

    I did not  homework6,I do not understand

---
+++ src/main/java/shipilovJava1/homeWork6/Dog.java
@@ -0,0 +1,10 @@
+package shipilovJava1.homeWork6;
+
+public class Dog extends Animal {
+    public Dog(String name, String animal, int run, int toSail, int noRun, int noToSail) {
+        super(name, animal,run,toSail,noRun,noToSail);
+    }
+
+
+
+}
